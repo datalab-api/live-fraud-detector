a6b309d2f695923ef475c940fa267dca5e81d676
mamadou cire GUISSE <mamadou.cire.guisse@kyndryl.com>
2022-09-27 09:37:20 +0200

    update public key

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ openssl genrsa -out certs/ca.key 2048
 openssl req -new -x509 -sha256 -days 1095 -subj "$API_DN/CN=CA" -key certs/ca.key -out certs/ca.pem
 
 # Admin
-openssl genrsa -out certs/admin-temp.key 2048
-openssl pkcs8 -inform PEM -outform PEM -in certs/admin-temp.key -topk8 -nocrypt -v1 PBE-SHA1-3DES -out certs/admin.key
+openssl genrsa -out certs/public.key 2048
+openssl pkcs8 -inform PEM -outform PEM -in certs/public.key -topk8 -nocrypt -v1 PBE-SHA1-3DES -out certs/admin.key
 openssl req -new -subj "$API_DN/CN=ADMIN" -key certs/admin.key -out certs/admin.csr
 openssl x509 -req -in certs/admin.csr -CA certs/ca.pem -CAkey certs/ca.key -CAcreateserial -sha256 -out certs/admin.pem
 
